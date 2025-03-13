38948eae06f2d57cc05b42895d71d41db782b970
Fajar Rizki Ramadhan <72005473+fajarrrX@users.noreply.github.com>
2025-03-13 15:34:36 +0700

    fix issues

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://rate-calculator.asdordigital.com.sg', 'https://rate-calculator.asdorstaging.com', 'https://dhl-rate-calculator-test.azurewebsites.net'],
 
     'allowed_origins_patterns' => [],
 
